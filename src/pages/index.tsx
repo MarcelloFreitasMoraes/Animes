a8f2eb6dda15de6f2e5a81c0086dbd94eaeba625
Marcelo Moraes <programadormarcello01@gmail.com>
2023-03-22 08:24:42 -0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,35 @@ import CardListComponent from "@/global/components/CardListComponent";
 import Slider from "@/global/components/Slider";
 import { Star } from "@/global/assets/Icons/Star";
 import { Like } from "@/global/assets/Icons/Like";
+import { DataProps, HomeProps } from "@/global/@types/type";
 
 
 
-export default function Home() {
- 
+export default function Home(props: HomeProps) {
+  const { sort, icon } = props;
+  const [data, setData] = useState<DataProps>();
+
+  let url = `https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]=0`;
+
+  if (sort === "user_count") {
+    url += "&sort=-user_count";
+  }
+
+  if (sort === "average_rating") {
+    url += "&sort=-average_rating";
+  }
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data.data);
+        console.log(response, "ress");
+      })
+      .catch(function (error) {
+        console.log(error.toJSON());
+      });
+  }, [url]);
 
   return (
     <S.Container>
